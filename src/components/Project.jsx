c1d23c0898e53b49a4b6910bb895e156303fc478
Tamil2411 <2004tamilselvan@gmail.com>
2025-07-31 13:12:26 +0530

    skills added

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,88 +1,3 @@
-// 'use client';
-// import React from 'react';
-// import { motion } from 'framer-motion';
-// import { FaLink } from 'react-icons/fa'; // link icon
-
-// const project = [
-//   {
-//     photo: "/Images/restaturant.jpeg",
-//     title: "PizzaBurg Restaurant",
-//     desc: "A modern food delivery website built with Next.js, featuring responsive design and smooth user navigation.",
-//     link: "https://pizzaburg-website.vercel.app/"
-//   },
-//   {
-//     photo: "/Images/image-slider.jpeg",
-//     title: "Image Slider",
-//     desc:  "A responsive image slider built using HTML, CSS, and JavaScript with smooth navigation controls.",
-//     link:  "https://image-slider-ten-lake.vercel.app/"
-//   },
-//   {
-//     photo: "/Images/weatherapp.jpeg",
-//     title: "Weather App",
-//     desc:  "A weather app built with React that shows real-time weather info based on user search.",
-//     link:  "https://weather-app-gamma-indol-22.vercel.app/"
-//   },
-//   {
-//     photo: "/Images/image-search.jpeg",
-//     title: "Image Search",
-//     desc:  "A simple image search web app built using HTML, CSS, and JavaScript to find photos by keyword.",
-//     link:  "https://image-search-engine-blush.vercel.app/"
-//   }
-// ];
-
-// const Project = () => {
-//   return (
-//     <section
-//       id="project"
-//       className="bg-[#1C1F35] text-white py-16 px-4 sm:px-6 md:px-10 lg:px-20 xl:px-32 2xl:px-40"
-//       style={{ minHeight: "100vh" }}
-//     >
-//       <div className="text-center mb-12">
-//         <h2 className="text-3xl sm:text-4xl md:text-5xl font-bold">
-//           My <span className="text-blue-400">Projects</span>
-//         </h2>
-//       </div>
-
-//       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-8 max-w-6xl mx-auto">
-//         {project.map((item, index) => (
-//           <motion.div
-//             key={index}
-//             initial={{ y: -50, opacity: 0 }}
-//             whileInView={{ y: 0, opacity: 1 }}
-//             transition={{ duration: 0.8, delay: index * 0.2 }}
-//             viewport={{ once: true }}
-//             className="relative group rounded-2xl overflow-hidden shadow-lg"
-//           >
-//             <img
-//               src={item.photo}
-//               alt={`Project ${index + 1}`}
-//               className="w-full h-[260px] object-cover group-hover:scale-110 transition-transform duration-500"
-//             />
-//             <div className="absolute inset-0 bg-black/30 group-hover:bg-black/60 transition-all duration-500"></div>
-
-//             {/* Hover content */}
-//             <div className="absolute inset-0 flex flex-col items-center justify-center opacity-0 group-hover:opacity-100 transition duration-500 text-center">
-//               <h3 className="text-xl font-semibold mb-4">{item.title}</h3>
-//               <p className="text-lg font-medium mb-4 w-[500px]">{item.desc}</p>
-//               <a
-//                 href={item.link}
-//                 target="_blank"
-//                 rel="noopener noreferrer"
-//                 className="bg-white text-[#1C1F35] px-4 py-2 rounded-full flex items-center gap-2 hover:bg-blue-400 hover:text-white transition"
-//               >
-//                 <FaLink />
-//                 View Project
-//               </a>
-//             </div>
-//           </motion.div>
-//         ))}
-//       </div>
-//     </section>
-//   );
-// };
-
-// export default Project;
-
 'use client';
 import React from 'react';
 import { motion } from 'framer-motion';
@@ -109,7 +24,7 @@ const project = [
   },
   {
     photo: "/Images/image-search.jpeg",
-    title: "Image Search",
+    title: "Image Search Engine",
     desc: "A simple image search web app built using HTML, CSS, and JavaScript to find photos by keyword.",
     link: "https://image-search-engine-blush.vercel.app/"
   }
@@ -119,7 +34,7 @@ const Project = () => {
   return (
     <section
       id="project"
-      className="bg-[#1C1F35] text-white py-16 px-4 sm:px-6 md:px-10 lg:px-20 xl:px-32 2xl:px-40"
+      className="bg-[#1C1F35] text-white py-15 px-4 sm:px-6 md:px-10 lg:px-20 xl:px-32 2xl:px-40"
       style={{ minHeight: "100vh" }}
     >
       <div className="text-center mb-12">
@@ -143,6 +58,7 @@ const Project = () => {
               alt={`Project ${index + 1}`}
               className="w-full h-[220px] sm:h-[260px] object-cover group-hover:scale-110 transition-transform duration-500"
             />
+            {/* <h1 className="p-2 text-center text-white text-md">{item.title}</h1> */}
             <div className="absolute inset-0 bg-black/30 group-hover:bg-black/60 transition-all duration-500"></div>
 
             {/* Hover content */}
