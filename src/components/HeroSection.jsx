91008f1a82a14760a3bfd2409790ea9d1df28a45
Tamil2411 <2004tamilselvan@gmail.com>
2025-08-17 12:22:10 +0530

    Resume added

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -58,7 +58,7 @@ const HeroSection = () => {
           {/* Resume Download Button */}
           <div className="flex justify-center md:justify-start">
             <a
-              href="/Images/Tamil resume.pdf"
+              href="/Images/TAMIL resume.pdf"
               download
               className="inline-flex items-center px-6 py-3 text-[#0F172A] bg-blue-400 rounded-full border border-transparent shadow-[0_4px_20px] shadow-[#3ABEF9]/40 hover:bg-transparent hover:text-white hover:border-[#3ABEF9] transition duration-300"
             >
